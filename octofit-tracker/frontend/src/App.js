d53da587a81bef61523b503bf46f318e5559b435
c. lefebvre <sidheandtea@outlook.com>
2025-04-08 01:30:14 +0000

    more style updates with added color

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -6,6 +6,7 @@ import Teams from './components/Teams';
 import Users from './components/Users';
 import Workouts from './components/Workouts';
 import './App.css';
+import logo from '../public/octofitapp-small.png'; // Import the logo
 
 function App() {
   return (
@@ -13,7 +14,10 @@ function App() {
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">OctoFit Tracker</Link>
+            <div className="logo">
+              <img src={logo} alt="OctoFit Logo" />
+              <Link className="navbar-brand" to="/">OctoFit Tracker</Link>
+            </div>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
