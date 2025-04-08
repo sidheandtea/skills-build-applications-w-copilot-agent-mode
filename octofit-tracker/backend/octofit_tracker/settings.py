3e34c926a1f76306bdda4a41ab8b7c37bcb7a6f6
c. lefebvre <sidheandtea@outlook.com>
2025-04-08 01:11:24 +0000

    setup django rest framework

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'your-secret-key'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'miniature-space-winner-4jqx4jgjjx55cjw67-8000.app.github.dev']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
